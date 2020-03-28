adcbb279925afbad5f505d727ed77b253152901b
analubarreto <analuibm@gmail.com>
2020-03-28 18:12:10 -0300

    added cors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 
+app.use(cors({
+  origin: 'http://localhost:5000' // which apps will be able to access our app
+}))
 app.use(express.json())
 app.use(routes)
-
-/**
- * Tipos de parâmetros
- * 
- * Query: parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
- * Route: parâmetros utilizados para identificar recursos
- * Request body: corpo da requisição, utilziando para criar ou alterar recursos
- */
 
 app.listen(8000)
